d31d45285bcd2f544ff4e4b8bd902fea1a31dcea
李宣哲 <xuanzheli@outlook.com>
2026-08-04 17:45:21 +0800

    Add files via upload

--- Scripts/Potion.cs
+++ Scripts/Potion.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Potion : MonoBehaviour
 {
+    public GameObject effectGo;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         LunaController lunaController = collision.GetComponent<LunaController>();
         if (lunaController != null && lunaController.CurrentHealth < lunaController.MaxHealth)
         {
             lunaController.ChangeHealth(1);
+            Instantiate(effectGo, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
         
