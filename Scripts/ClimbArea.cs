4ebe6615d2eef285ec3fc12ea3bfc4cab435883f
李宣哲 <xuanzheli@outlook.com>
2026-08-17 17:58:16 +0800

    Add files via upload

--- Scripts/ClimbArea.cs
+++ Scripts/ClimbArea.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ClimbArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Luna"))
+        {
+            collision.GetComponent<LunaController>().Climb(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Luna"))
+        {
+            collision.GetComponent<LunaController>().Climb(false);
+        }
     }
 }
